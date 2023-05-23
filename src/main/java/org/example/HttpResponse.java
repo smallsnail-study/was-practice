d7ffdaefa92b10abb31a1e0795511ac5e75b7b97
yunhuiGwak <kwakyh17@naver.com>
2023-05-23 15:55:55 +0900

    :sparkles: HttpResponse 응답메시지 구현 클래스 - operand1=11&operator=*&operand2=55

---
+++ src/main/java/org/example/HttpResponse.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class HttpResponse {
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
+
+    private final DataOutputStream dos;
+
+    public HttpResponse(DataOutputStream dos) {
+        this.dos = dos; // DataOutputStream을 생성자를 통해 받는다.
+    }
+
+    public void response200Header(String contentType, int lengthOfBodyContent) {    // Header 값 세팅(Header + Blank line)
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void responseBody(byte[] body) { // Body값 세팅
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
