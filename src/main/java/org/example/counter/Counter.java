3c5597a59674610a9bf158b76d229cd489715296
yunhuiGwak <kwakyh17@naver.com>
2023-05-24 15:32:46 +0900

    :sparkles: 싱글톤에서 상태를 유지하게 설계한 경우 - 멀티스레드 환경에서 1개의 자원을 공유하게 되면 원치않는 결과(RaceCondition)가 나온다. - Thread safety 하지 않음 → 원치않는 결과(RaceCondition)가 발생할 수 있다.

---
+++ src/main/java/org/example/counter/Counter.java
@@ -0,0 +1,27 @@
+package org.example.counter;
+
+public class Counter implements Runnable {
+    // 상태를 유지하도록 설계
+    private int count = 0;  // 상태값
+
+    public void increment() {
+        count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    public int getValue() {
+        return count;
+    }
+
+    // 스레드
+    @Override
+    public void run() {
+        this.increment();
+        System.out.println("Value for Thread After increment " + Thread.currentThread().getName() + " " + this.getValue()); //1
+        this.decrement();
+        System.out.println("Value for Thread at last " + Thread.currentThread().getName() + " " + this.getValue()); //0
+    }
+}
