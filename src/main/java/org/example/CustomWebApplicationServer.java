63e075b1a884e833225d0e2f308d19f033b33bbc
yunhuiGwak <kwakyh17@naver.com>
2023-05-24 14:19:21 +0900

    :zap: Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다. - Step2의 단점 보완

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -1,18 +1,20 @@
 package org.example;
 
-import org.example.calculator.domain.Calculator;
-import org.example.calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
 
     private final int port;
+
+    // ThreadPool 적용
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     public CustomWebApplicationServer(int port) {
         this.port = port;
@@ -34,10 +36,9 @@ public class CustomWebApplicationServer {
                 logger.info("[CustomWebApplicationServer] client connected!");
 
                 /**
-                 * Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리
-                 * 하도록 한다.
+                 * Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
                  */
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
